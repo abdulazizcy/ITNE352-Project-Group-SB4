aa1501cb91c284b6ce519352d1b7570418354e09
Abdulaziz Mohd Abdulaziz <abdulazizmohd341125@gmail.com>
2025-05-15 20:42:28 +0300

    first commit

--- Server.py
+++ Server.py
@@ -13,6 +13,26 @@ def connection (socka):
     print('THE CLIENT ',client_name,' IS CONNECTED.')
 
 
-while True:
+#while True:
 
-    request_num=socka.recv(2048).decode('ascii')
+   # request_num=socka.recv(2048).decode('ascii')
+
+
+
+
+
+
+#Creating new tcp socket and assign full internet address to it  (ip address and port number )
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock_c:
+    sock_c.bind(("127.0.0.1" , 49999))
+
+#listening for clients connection 
+    sock_c.listen()
+
+#Accepting multiple connection from different client through the while loop 
+while True:
+    socka , sockname = sock_c.accept()
+#Thread that handle clients requests 
+t=threading.Thread(target=connection , args=(socka , sockname ))
+t.start() 
+
